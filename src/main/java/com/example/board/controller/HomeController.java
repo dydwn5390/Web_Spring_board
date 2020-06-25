97fdffaeab6c5e175364a50a11fcb0af12674c95
yjlee <dydwn5390@gmail.com>
2020-06-25 13:40:39 +0900

    3.14 p.324 까지 완료

--- src/main/java/com/example/board/controller/HomeController.java
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+//초기화 되지 않은 final 필드나, @NonNull 이 붙은 필드에 대해 생성자를 생성해 줌
 @RequiredArgsConstructor
 public class HomeController {
     private final BoardMapper boardMapper;
